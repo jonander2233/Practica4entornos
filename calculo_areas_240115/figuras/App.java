7c4b8d5cea510dbc49678a09ebc6697f58b28b8c
jon ander aranguren <jonander2233@gmail.com>
2024-01-24 19:28:37 +0100

    sustitucion de print

--- calculo_areas_240115/figuras/App.java
+++ calculo_areas_240115/figuras/App.java
@@ -7,7 +7,8 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         int option;
         do {
-            System.out.println("Calculadora de Área - Seleccione una opción:");
+            System.out.println("Calculadora de Área by Jon Ander");
+	    System.out.println("Selecciona una opción");
             System.out.println("1. Círculo");
             System.out.println("2. Cuadrado");
             System.out.println("3. Triángulo");
